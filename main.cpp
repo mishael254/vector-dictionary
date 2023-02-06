b560438ed9cddbb6033fee8fed19f4a2c864a1e7
mishael254 <mishaelmishantoz@gmail.com>
2023-02-06 10:06:46 +0300

    include the following directives iostream, vector, algorithm

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(){
     for (string word;cin>>word;) {
         words.push_back(word);//the new elements are read and stored in a vector
         cout<<"the number of words"<<words.size()<<endl;
-        //sort the elements in the vector
+        //sort the elements in the vector called words
         sort(words.begin(),words.end());
 
         for(int i=0;i<words.size();++i){
